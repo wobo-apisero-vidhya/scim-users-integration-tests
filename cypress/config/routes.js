a9fc425972f76cd38ffab81fd0558bd66b5e99b0
SandeshChavan <46970114+SandeshChavan@users.noreply.github.com>
2022-01-25 15:13:54 +0530

    GET Update

--- cypress/config/routes.js
+++ cypress/config/routes.js
@@ -1,8 +1,17 @@
 export const routes={
     POST: '/scim/Users',
-    GET: '/scim/Users/55',
+    
+    GET: '/scim/Users/1',
+    GET_ALL_USERS: '/scim/Users',
+    GET_INVALID_ID: '/scim/Users/abc',
     GET_FILTER: 'scim/Users?filter=userName eq wobo-employee1@wobodev.com',
+    GET_FILTER_EMPTY_RES: 'scim/Users?filter=userName eq wobo-employee60@wobodev.com',
+    GET_INVALID_FILTER: 'scim/Users?filter=noName eq wobo-employee60@wobodev.com',
+    
     UPDATE: 'scim/Users/55',
+    
     DISABLE: 'scim/Users/55/',
-    DELETE: 'scim/Users/55'
+    
+    DELETE: 'scim/Users/51',
+    DELETE_INVALID_ID: 'scim/Users/55'
 }
--- cypress/integration/tests/users/deleteUsers.spec.js
+++ cypress/integration/tests/users/deleteUsers.spec.js
@@ -5,11 +5,23 @@ import auth from "../../../config/auth";
 import { createRequestBody, disableRequestBody, updateRequestBody } from "../../../fixtures/request/user";
 
 describe('DELETE :: Delete User', () => {
-  it('Deletes a user successfully', () => {
-    
+ 
+  it('Deletes a user successfully', () => {   
     cy.api({
       method: 'DELETE', 
       url: routes.DELETE,
+      auth: auth,
+    }).then((res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.not.be.null;
+      expect(res.body.id).to.equal('55');
+    });
+  });
+
+  it('Delete - unsuccessful response', () => {   
+    cy.api({
+      method: 'DELETE', 
+      url: routes.DELETE_INVALID_ID,
       auth: auth,
     }).then((res) => {
       expect(res.status).to.equal(200);
