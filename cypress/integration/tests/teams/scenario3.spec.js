3a761405ed0a1277d74e4accb70aa86f8d9d939c
shikhar d dixit <shikhar.d.dixit@apiseroinc.onmicrosoft.com>
2022-02-09 09:07:21 +0530

    create scenario setup not completed

--- cypress/integration/tests/teams/scenario3.spec.js
+++ cypress/integration/tests/teams/scenario3.spec.js
@@ -11,12 +11,41 @@ let user_details = {
 };
 
 before('create new test users and form an heirarchy', () => {
-    async.forEachSeries(dynamicTeamUsers, (user, done) => {
-        var newRequestBody = createRequestBody
-        newRequestBody.userName = user.userName
-        newRequestBody.emails[0].value = user.email
-        newRequestBody.name.familyName = user.familyName
-        newRequestBody.name.givenName = user.givenName
+  async.forEachSeries(dynamicTeamUsers, (user, cb) => {
+    var newRequestBody = {
+      schemas: [],
+      externalId: "",
+      userName: "",
+      active: null,
+      emails: [],
+      meta: null,
+      name: null,
+      roles: []
+    }
+        var newUser = {
+          userName: "",
+          email: "",
+          familyName: "",
+          givenName: ""
+        }
+
+        newRequestBody.schemas = createRequestBody.schemas;
+        newRequestBody.externalId = createRequestBody.externalId;
+        newRequestBody.active = createRequestBody.active;
+        newRequestBody.emails = createRequestBody.emails;
+        newRequestBody.meta = createRequestBody.meta;
+        newRequestBody.name = createRequestBody.name;
+
+        newUser.userName = user.userName;
+        newUser.email = user.email;
+        newUser.familyName = user.familyName;
+        newUser.givenName = user.givenName;
+
+        newRequestBody.userName = newUser.userName
+        newRequestBody.emails[0].value = newUser.email
+        newRequestBody.name.familyName = newUser.familyName
+        newRequestBody.name.givenName = newUser.givenName
+
         cy.api({
           method: "POST",
           url: routes.POST,
@@ -28,12 +57,13 @@ before('create new test users and form an heirarchy', () => {
           created_users[count] = user_details;
           count = count + 1;
         });
+        cb()
       });
 });
 
 describe('check user details', () => {
   it('should rectify the user ids and usernames', () => {
-      expect(created_users[0].id).to.equal('79');
+      expect(created_users[0].id).to.equal('78');
       expect(created_users[0].username).to.equal("");
     });
 });
